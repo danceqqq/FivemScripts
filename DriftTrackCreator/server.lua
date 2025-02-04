a6b80a20536cbe9e797527a2db44f7e3dde56ce6
danceq <140273750+danceqqq@users.noreply.github.com>
2025-02-04 12:45:58 +0300

    Update server.lua

--- DriftTrackCreator/server.lua
+++ DriftTrackCreator/server.lua
@@ -4,17 +4,35 @@ local leaderboards = {}
 
 -- Сохранение новой трассы
 RegisterServerEvent("saveDriftRace")
-AddEventHandler("saveDriftRace", function(start, clips, finish)
+AddEventHandler("saveDriftRace", function(start, clips, finish, name)
     local src = source
     local raceId = tostring(start)
     races[raceId] = {
         startPoint = start,
         clipPoints = clips,
-        finishPoint = finish
+        finishPoint = finish,
+        name = name
     }
     leaderboards[raceId] = {}
-    TriggerClientEvent("updateClientRaceData", -1, start, clips, finish) -- Обновляем данные у всех игроков
-    TriggerClientEvent("chat:addMessage", source, { args = { "^2Трасса успешно создана!" } })
+    TriggerClientEvent("updateClientRaceData", -1, start, clips, finish, name) -- Обновляем данные у всех игроков
+    TriggerClientEvent("chat:addMessage", src, { args = { "^2Трасса успешно создана!" } })
+end)
+
+-- Обновление существующей трассы
+RegisterServerEvent("updateDriftRace")
+AddEventHandler("updateDriftRace", function(start, clips, finish, name)
+    local src = source
+    local raceId = tostring(start)
+    if races[raceId] then
+        races[raceId].startPoint = start
+        races[raceId].clipPoints = clips
+        races[raceId].finishPoint = finish
+        races[raceId].name = name
+        TriggerClientEvent("updateClientRaceData", -1, start, clips, finish, name) -- Обновляем данные у всех игроков
+        TriggerClientEvent("chat:addMessage", src, { args = { "^2Трасса успешно обновлена!" } })
+    else
+        TriggerClientEvent("chat:addMessage", src, { args = { "^1Трасса не найдена!" } })
+    end
 end)
 
 -- Получение таблицы лидеров для трассы
@@ -36,6 +54,13 @@ AddEventHandler("updateLeaderboard", function(start, points)
         leaderboards[raceId][playerName] = points
         TriggerClientEvent("updateLeaderboardUI", -1, raceId, leaderboards[raceId]) -- Обновление для всех игроков
     end
+end)
+
+-- Синхронизация прохождения чекпоинтов
+RegisterServerEvent("syncCheckpoint")
+AddEventHandler("syncCheckpoint", function(start, checkpoint)
+    local raceId = tostring(start)
+    TriggerClientEvent("syncCheckpointUI", -1, raceId, checkpoint)
 end)
 
 -- Экспорт данных при остановке ресурса
